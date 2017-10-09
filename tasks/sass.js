4fca3c2a2340d2ba42a3622118238f9dd6bc5921
Adam Simpson <adam@heysparkbox.com>
2017-10-09 09:12:39 -0400

    fix: for some reason we're rendering every scss file as a css file

--- tasks/sass.js
+++ tasks/sass.js
@@ -9,31 +9,35 @@ const inform = require('./inform');
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 const PATHS = {
-  src: globby.sync('./styles/*.scss'),
-  dest: './dist/css/'
+  src: globby.sync('./styles/base.scss'),
+  dest: './dist/css/',
 };
 
-const getDest = (file) => {
+const getDest = file => {
   const fileName = path.basename(file, '.scss');
   return `${path.join(PATHS.dest, fileName)}.css`;
 };
 
 const execSass = (src, outputStyle, includePaths, sourceMap) => {
-  src.forEach((file) => {
+  src.forEach(file => {
     const dest = getDest(file);
-    shell.exec(`node-sass ${file} ${outputStyle} ${includePaths} ${sourceMap} ${dest}`);
+    shell.exec(
+      `node-sass ${file} ${outputStyle} ${includePaths} ${sourceMap} ${dest}`
+    );
   });
 };
 
 function sass() {
   inform.start('Running Sass');
-  const outputStyle = `--output-style ${(PRODUCTION ? 'compressed' : 'expanded')}`;
+  const outputStyle = `--output-style ${PRODUCTION
+    ? 'compressed'
+    : 'expanded'}`;
   const sourceMap = PRODUCTION ? '' : '--source-map true';
 
   shell.exec(`mkdir -p ${PATHS.dest}`);
 
   execSass(PATHS.src, outputStyle, '', sourceMap);
   return inform.done();
-};
+}
 
 sass();
